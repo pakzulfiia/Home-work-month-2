f0060a7c81e437a4bd3e52f0d5c855806133a283
pakzulfiia <pakzulfiia@gmail.com>
2026-04-20 17:37:03 +0600

    something

---
+++ home_work/homework7.py
@@ -0,0 +1,25 @@
+def create_table(conn):
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS books(
+    name TEXT,
+    author TEXT,
+    publition_year INTEGER,
+    genre TEXT,
+    number_of_pages INTEGER,
+    number_of_copies INTEGER
+    )
+""")
+    
+
+def insert_books(conn, name, author, publition_year, genre, number_of_pages, number_of_copies):
+    conn.execute("""
+INSERT INTO books 
+VALUES (?, ?, ?, ?, ?, ?)""",
+(name, author, publition_year, genre, number_of_pages, number_of_copies))
+    conn.commit()
+
+def get_all_books(conn):
+    result = conn.execute("""
+    SELECT * FROM books
+""")
+    return result.fetchall()
